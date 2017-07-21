ff33a707b6bc851bb37c9ded293076678f36c28f
luckyPhilJ <phil.richards@mac.com>
2017-07-21 12:29:39 +0900

    first commit

---
+++ command.sh
@@ -0,0 +1,4 @@
+pip install -r floyd_requirements.txt
+cp -r /code/* /output
+cd /output
+/run_jupyter.sh --no-browser --NotebookApp.base_url='/notebooks/ogJsvBEkB8qpKbjtZXTYcF' --NotebookApp.token='' --NotebookApp.allow_origin='*'
